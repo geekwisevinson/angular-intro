791b001b3c5f76e86dd0fabdb1228a9343623827
Jordan Mavrogeorge <jordomav@gmail.com>
2019-05-30 19:12:58 -0700

    day 4 break

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { CHARACTERS } from './mock-characters';
 import { CharacterInterface } from '../interfaces/character-interface';
@@ -11,13 +12,26 @@ import { Character } from '../models/character';
 })
 export class CharactersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
-  characters: CharacterInterface[] = CHARACTERS;
+  characters: CharacterInterface[];
+
+  type: string | boolean;
 
   isHero(character: CharacterInterface): boolean { return character.type === "Hero"; }
 
+  renderCharacters() {
+    this.type = this.route.snapshot.queryParamMap.get("type");
+    if (this.type) {
+      this.characters = CHARACTERS.filter(character => character.type === this.type);
+    } else {
+      this.type = false;
+      this.characters = CHARACTERS;
+    }
+  }
+
   ngOnInit() {
+    this.renderCharacters();
   }
 
 }
